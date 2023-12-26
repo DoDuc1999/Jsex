bc1c2c5ea738d4608a5bbe4dd57a253de38c1446
DoDuc1999 <ducdovan59@gmail.com>
2023-12-27 00:38:24 +0700

    q1

---
+++ q2.js
@@ -0,0 +1,6 @@
+var listSv = [
+    {name : 'Do Duc', age: '21', gender: 'Male'},
+    {name : 'Do Duc2', age: '25', gender: 'Male'},
+    {name : 'Do Duc3', age: '23', gender: 'Male'},
+    {name : 'Do Duc4', age: '22', gender: 'Male'},
+]
